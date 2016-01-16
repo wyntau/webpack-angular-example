fb7ab55098bc8098fa9c81147a7860e48ba281b3
Treri <treri.liu@gmail.com>
2016-01-16 13:49:30 +0800

    update html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path')
   , webpack = require('webpack')
 
   , ReplaceTaskWebpackPlugin = require('replace-task-webpack-plugin')
+  , htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   resolve: {
@@ -19,19 +20,23 @@ module.exports = {
     filename: '[name].js',
     pathinfo: true
   },
-  // plugins: [
-  //   new ReplaceTaskWebpackPlugin({
-  //     patterns: [{
-  //       match: 'foo',
-  //       replacement: 'foo+bar'
-  //     }]
-  //   }),
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress: {
-  //       warnings: false
-  //     }
-  //   })
-  // ],
+  plugins: [
+    new ReplaceTaskWebpackPlugin({
+      patterns: [{
+        match: 'foo',
+        replacement: 'foo+bar'
+      }]
+    })
+    // , new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // })
+    // , new htmlWebpackPlugin({
+    //   filename: '../index.html',
+    //   template: 'index.temp.html'
+    // })
+  ],
   module: {
     loaders: [
       {
